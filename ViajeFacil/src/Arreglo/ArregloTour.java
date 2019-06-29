b5b8c45cd933f00628ca08954815cb90ca9e075b
Samil96 <1628112@utp.edu.pe>
2019-06-28 19:45:46 -0500

    subiendo Arreglo de Tour

--- ViajeFacil/src/Arreglo/ArregloTour.java
+++ ViajeFacil/src/Arreglo/ArregloTour.java
@@ -5,10 +5,25 @@
  */
 package Arreglo;
 
+import clases.Tour;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Nuria
  */
 public class ArregloTour {
+    private List<Tour> tour;
+
+    public ArregloTour(List<Tour> tour) {
+        tour=new ArrayList();
+        tour.add(new Tour("28/07/19", "30/07/19", tour, 20, "Tour del Sol"));
+        tour.add(new Tour("27/08/19", "31/08/19", tour, 20, "Tour de Santa Rosa Lima"));
+        tour.add(new Tour("25/09/19", "30/09/19", tour, 20, "Camino Inca"));
+        
+    }
+    
+    
     
 }
